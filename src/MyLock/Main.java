17d14b6c1c69a8ef8f9fcaf7830fe58509e2ce2d
huashen <2494946808@qq.com>
2025-03-16 04:18:50 +0800

    公平锁实现

---
+++ src/MyLock/Main.java
@@ -0,0 +1,29 @@
+package MyLock;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int[] count = new int[]{1000};
+        List<Thread> threads = new ArrayList<>();
+        MyLock lock = new MyLock();
+        for(int i=0;i<100;i++){
+            threads.add(new Thread(()->{
+                lock.lock();
+                for(int i1=0;i1<10;i1++){
+                    count[0]--;
+                }
+                lock.unlock();
+            }));
+        }
+        for(Thread thread:threads){
+            thread.start();
+        }
+        for(Thread thread:threads){
+            thread.join();
+        }
+        System.out.println(count[0]);
+    }
+}
---
+++ src/MyLock/MyLock.java
@@ -0,0 +1,61 @@
+package MyLock;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.LockSupport;
+
+public class MyLock {
+    AtomicBoolean flag = new AtomicBoolean(false);
+
+    Thread owner = null;
+
+    AtomicReference<Node> head = new AtomicReference<>(new Node());
+    AtomicReference<Node> tail = new AtomicReference<Node>(head.get());
+
+    void lock() {
+        Node current = new Node();
+        current.thread = Thread.currentThread();
+
+        while(true){
+            Node currentTail = tail.get();
+            if(tail.compareAndSet(currentTail, current)){
+                current.pre = currentTail;
+                currentTail.next = current;
+                break;
+            }
+        }
+        while (true) {
+
+            if (current.pre == head.get() && flag.compareAndSet(false, true)) {
+                owner = Thread.currentThread();
+                head.set(current);
+                current.pre.next = null;
+                current.pre = null;
+
+                return;
+            }
+
+            LockSupport.park();
+        }
+    }
+
+        void unlock () {
+            if (Thread.currentThread() != owner) {
+                throw new IllegalStateException("当前线程并没有锁");
+            }
+            Node headNode = head.get();
+            Node next = headNode.next;
+            flag.set(false);
+            if(next !=null){
+                LockSupport.unpark(next.thread);
+            }
+
+        }
+
+        class Node {
+            Node pre;
+            Node next;
+            Thread thread;
+        }
+    }
+
